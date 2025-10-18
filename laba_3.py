cb67f26c9f397892384a1b5651716332b001e755
ykrop4ek <d.semenyakov.82@gmail.com>
2025-10-18 08:55:55 +0300

    Add task 3, laba_3.py

--- laba_3.py
+++ laba_3.py
@@ -4,5 +4,9 @@ squares = [x**2 for x in range(1, 11)]
 
 #создание списка только четных чисел из диапазона от 1 до 20
 
-honest = [x for x in range(0,21,2)][1::]
+honest = [x for x in range(0,20,2)][1::]
 # print(honest)
+
+words = ["python", "Java", "c++", "Rust", "go"]
+up_words = [w.upper() for w in words if len(w) > 3]
+# print(up_words)
