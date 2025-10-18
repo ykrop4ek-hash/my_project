00b6f2fbf9873b0b2bd188d0a7aa64684aa56e34
ykrop4ek <d.semenyakov.82@gmail.com>
2025-10-18 08:53:11 +0300

    Add task 2, laba_3.py

--- laba_3.py
+++ laba_3.py
@@ -1,4 +1,8 @@
 # Создание списка квадратов чисел от 1 до 10
 squares = [x**2 for x in range(1, 11)]
+# print(squares)
 
-print(squares)
+#создание списка только четных чисел из диапазона от 1 до 20
+
+honest = [x for x in range(0,21,2)][1::]
+# print(honest)
