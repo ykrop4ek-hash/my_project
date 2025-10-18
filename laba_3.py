101e50cde024b7ac3409e2fd2261f2da996316a5
ykrop4ek <d.semenyakov.82@gmail.com>
2025-10-18 08:59:45 +0300

    Add task 4, laba_3.py

--- laba_3.py
+++ laba_3.py
@@ -10,3 +10,18 @@ honest = [x for x in range(0,20,2)][1::]
 words = ["python", "Java", "c++", "Rust", "go"]
 up_words = [w.upper() for w in words if len(w) > 3]
 # print(up_words)
+
+class Countdown:
+    def __init__(self, n):
+        self.n = n
+
+    def __iter__(self):
+        current = self.n
+        while current >= 1:
+            yield current
+            current -= 1
+
+
+# Пример использования
+for x in Countdown(5):
+    print(x)
